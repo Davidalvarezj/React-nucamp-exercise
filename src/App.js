0f5679591b194355bc40cbce2b488760a42cef0d
David Alvarez-mac <davidalvarezj@gmail.com>
2022-12-03 18:36:18 -0500

    Adding header and footer first steps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer"
+import CampsitesList from "./features/campsites/CampsitesList";
+import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-      Hello David!
+      <Header />
+      <CampsitesList />
+      <Footer/>
     </div>
   );
 }
